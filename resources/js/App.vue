99d0640c987ad95430bfdd468bf3c70e8061c73e
Abdul Majid <abdulmajidcse@gmail.com>
2023-01-22 14:49:53 +0600

    init app

---
+++ resources/js/App.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    data() {
+        return {
+            count: 0,
+        };
+    },
+};
+</script>
+
+<template>
+    <h2>Laravel Vuejs Boilerplate</h2>
+    <button @click="count++">Count is: {{ count }}</button>
+</template>
